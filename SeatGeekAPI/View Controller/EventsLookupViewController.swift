0199d6aee90d8fd368b12627debe82db8db9d29e
Hin Wong <hinwong@Hins-MacBook-Pro.local>
2021-07-11 14:46:11 -0400

    finished events vc connections

--- SeatGeekAPI/View Controller/EventsLookupViewController.swift
+++ SeatGeekAPI/View Controller/EventsLookupViewController.swift
@@ -11,7 +11,26 @@ class EventsLookupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        eventsTableView.delegate = self
+        eventsSearchBar.delegate = self
     }
     
+    @IBOutlet var eventsTableView: UITableView!
+    @IBOutlet weak var eventsSearchBar: UISearchBar!
+}
 
+extension EventsLookupViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+
+extension EventsLookupViewController: UISearchBarDelegate {
+    
 }
